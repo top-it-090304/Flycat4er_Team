6eb6b7e1d2d8a8b43d1d968ce0b42a418b9b2eb6
Fedorenkov Evgeny <evgenyfedorenkov744@gmail.com>
2026-03-05 22:14:41 +0600

    work with backgroud

--- level.gd
+++ level.gd
@@ -1,15 +1,12 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
-func _on_exit_area_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+func _on_bottom_body_entered(body: Node2D) -> void:
+	if body.name == "Mini_Mario":
+		get_tree().change_scene_to_file("res://menu.tscn")
