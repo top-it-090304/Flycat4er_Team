0217a300ffa8c72da6cc604caa2da04c1b7be2c8
Fedorenkov Evgeny <evgenyfedorenkov744@gmail.com>
2026-03-12 23:50:31 +0600

    Player movement upgrade

--- Mario_player/Mini_Mario.gd
+++ Mario_player/Mini_Mario.gd
@@ -3,25 +3,26 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 
-const JUMP_VELOCITY = -342.0
-
-const acc = 50
-const frocton = 70
+const JUMP_VELOCITY = -310.0
 
 var score = 0
+
+var gravity = 750.0
 
 var health = 1
 
 @onready var anim = $AnimatedSprite2D
 
-func _physics_process(delta: float) -> void:
+func _physics_process(delta):
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += gravity * delta
 
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		anim.play("Jump_mini")
-
+	
+	if Input.is_action_just_released("ui_accept") and velocity.y < 0:
+		velocity.y *= 0.4
+		
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction > 0 and health > 0:
 		$AnimatedSprite2D.flip_h = false
