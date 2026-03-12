123be7aab3715cb4101f70a47908eb616a2efeee
Fedorenkov Evgeny <evgenyfedorenkov744@gmail.com>
2026-03-12 22:22:36 +0600

    recovery

--- menu.gd
+++ menu.gd
@@ -7,9 +7,8 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func _on_button_pressed() -> void:
+func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://level.tscn")
-
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
