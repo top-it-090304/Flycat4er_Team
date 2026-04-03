00eaa8a6ec03f5725d37a5b41f3bef28d3036921
Fedorenkov Evgeny <evgenyfedorenkov744@gmail.com>
2026-04-03 10:48:37 +0600

    Platform

--- platform_a.gd
+++ platform_a.gd
@@ -3,7 +3,7 @@ extends AnimatableBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$"../AnimationPlayer".play("platfor_anim")
+	$"../AnimationPlayer".play("platform_anim")
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
