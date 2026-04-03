2470b8a3baa71ba399ad37a2c0c2e3e76e8e3b85
Fedorenkov Evgeny <evgenyfedorenkov744@gmail.com>
2026-04-03 11:20:35 +0600

    Platform

--- exit_lvl_2.gd
+++ exit_lvl_2.gd
@@ -1,9 +1,17 @@
-extends AnimatableBody2D
+extends Area2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$"../AnimationPlayer".play("platform_anim")
+	pass # Replace with function body.
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Mini_Mario":
+		get_tree().change_scene_to_file("res://level_3.tscn")
--- platforms_2.gd
+++ platforms_2.gd
@@ -1,9 +1,11 @@
-extends AnimatableBody2D
+extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$"../AnimationPlayer".play("platform_anim")
+	$AnimationPlayer.play("platform_anim_2")
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
